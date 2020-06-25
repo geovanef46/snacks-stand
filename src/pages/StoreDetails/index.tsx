999ef5f7438f00c1e05633b34b3c327c1f7f63e0
Joaquim <juakacc@gmail.com>
2020-06-25 20:56:23 -0300

    Get store information

--- src/pages/StoreDetails/index.tsx
+++ src/pages/StoreDetails/index.tsx
@@ -6,120 +6,126 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonText,
 } from "@ionic/react";
 
 import Header from "../../components/Header";
 import ShowRating from "../../components/ShowRating";
 import Snack from "../../models/Snack";
 import SnackItem from "../../components/SnackItem";
+import { useParams } from "react-router";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
-const defaultSnacks: Snack[] = [
-  {
-    id: 1,
-    name: "Pastel de Frango",
-    description: "Pastel de frango katupiry",
-    price: 4,
-  },
-  {
-    id: 2,
-    name: "Pastel de Frango",
-    description: "Pastel de frango katupiry",
-    price: 4,
-  },
-  {
-    id: 3,
-    name: "Pastel de Frango",
-    description: "Pastel de frango katupiry",
-    price: 4,
-  },
-  {
-    id: 4,
-    name: "Pastel de Frango",
-    description: "Pastel de frango katupiry",
-    price: 4,
-  },
-  {
-    id: 5,
-    name: "Pastel de Frango",
-    description: "Pastel de frango katupiry",
-    price: 4,
-  },
-  {
-    id: 6,
-    name: "Pastel de Frango",
-    description: "Pastel de frango katupiry",
-    price: 4,
-  },
-];
+const GET_SNACKS = gql`
+  query GetSnacks($storeId: ID!) {
+    store(id: $storeId) {
+      id
+      name
+      description
+      phone
+      owner {
+        id
+        name
+      }
+      address {
+        id
+        street
+        number
+        complement
+        district
+        city
+        state
+      }
+      snacks {
+        id
+        name
+        description
+      }
+    }
+  }
+`;
 
 const StoreDetails = () => {
-  const [snacks, setSnacks] = useState<Snack[]>(defaultSnacks);
+  const { id: storeId } = useParams<{ id: string }>();
+
+  const { loading, error, data } = useQuery(GET_SNACKS, {
+    variables: {
+      storeId,
+    },
+  });
 
   return (
     <IonPage>
       <Header />
       <IonContent>
         <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonAvatar>
-                <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
-              </IonAvatar>
-              <ShowRating starRate={5} />
-            </IonCol>
-            <IonCol>
-              <h3>Júnior Lanches</h3>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <p>
-                Nós vendemos lanches baratinhos, entregamos na sua casa com
-                segurança e higiene
-              </p>
-              <p>
-                <strong>Proprietário:</strong> Júnior de Antônio de Elvira
-              </p>
-
-              <p>Endereço: Rua do Berra Bode</p>
-              <p>Telefone: 394893489348</p>
-              <p>Entrega grátis? A partir de R$ 10</p>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>Mais Comprados</IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              {snacks.map((value, index) => {
-                return (
-                  <SnackItem
-                    snack={value}
-                    key={index}
-                    removeSnack={() => {}}
-                    isAdd={true}
-                  />
-                );
-              })}
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>Novidades</IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              {snacks.map((value, index) => {
-                return (
-                  <SnackItem
-                    snack={value}
-                    key={index}
-                    removeSnack={() => {}}
-                    isAdd={true}
-                  />
-                );
-              })}
-            </IonCol>
-          </IonRow>
+          {loading || error ? (
+            <IonText>Carregando ou ERROR...</IonText>
+          ) : (
+            <>
+              <IonRow>
+                <IonCol>
+                  <IonAvatar>
+                    <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+                  </IonAvatar>
+                  <ShowRating starRate={5} />
+                </IonCol>
+                <IonCol>
+                  <h3>{data?.store.name}</h3>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  <p>{data?.store.description}</p>
+                  <p>
+                    <strong>Proprietário:</strong> {data?.store.owner.name}
+                  </p>
+                  <p>
+                    Endereço: {data?.store.address.street},{" "}
+                    {data?.store.address.number}, {data?.store.address.district}
+                    , {data?.store.address.city}
+                  </p>
+                  <p>Telefone: {data?.store.phone}</p>
+                  <p>Entrega grátis? PENSAR</p>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Mais Comprados</IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  {data?.store.snacks.map((snack: Snack) => {
+                    return (
+                      <SnackItem
+                        snack={snack}
+                        key={`m-${snack.id}`}
+                        removeSnack={() => {}}
+                        isAdd={true}
+                      />
+                    );
+                  })}
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Novidades</IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  {data?.store.snacks.map((snack: Snack) => {
+                    return (
+                      <SnackItem
+                        snack={snack}
+                        key={`new-${snack.id}`}
+                        removeSnack={() => {}}
+                        isAdd={true}
+                      />
+                    );
+                  })}
+                </IonCol>
+              </IonRow>
+            </>
+          )}
         </IonGrid>
       </IonContent>
     </IonPage>
